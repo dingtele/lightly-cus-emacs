7cd18642bf12c40449a0c9d48faad46da99a54f5
dingtele <sardine323@gmail.com>
2024-05-10 20:25:49 +0800

    05103

--- var/save-place.el
+++ var/save-place.el
@@ -1,19 +1,5 @@
 ;;; -*- coding: utf-8; mode: lisp-data -*-
-(("/Users/dingyu/.emacs.d/init.el" . 2005)
- ("/Users/dingyu/.emacs.d/customizations/editing.el" . 4816)
- ("/Users/dingyu/.emacs.d/customizations/init-org.el" . 1051)
- ("/Users/dingyu/.emacs.d/customizations/navigation.el" . 2299)
- ("/Users/dingyu/.emacs.d/customizations/ui.el" . 1027)
- ("/Users/dingyu/.emacs.d/customizations/misc.el" . 1099)
- ("/opt/homebrew/Cellar/emacs-plus@29/29.1/share/emacs/29.1/lisp/progmodes/elisp-mode.el" . 55325)
- ("/opt/homebrew/Cellar/emacs-plus@29/29.1/share/emacs/29.1/lisp/subr.el" . 96784)
- ("/Users/dingyu/.emacs.d/customizations/elisp-editing.el" . 240)
- ("/Users/dingyu/.emacs.d/customizations/setup-clojure.el" . 335)
- ("/Users/dingyu/.emacs.d/var/bing-dict/"
-  (dired-filename . "/Users/dingyu/.emacs.d/var/bing-dict/vocabulary.org"))
- ("/Users/dingyu/.emacs.d/var/bing-dict/vocabulary.org" . 724)
- ("/Users/dingyu/.emacs.d/"
-  (dired-filename . "/Users/dingyu/.emacs.d/.DS_Store"))
- ("/Users/dingyu/.emacs.d/.git/"
-  (dired-filename . "/Users/dingyu/.emacs.d/.git/COMMIT_EDITMSG"))
- ("/Users/dingyu/.m2/settings.xml" . 1281))
+(("c:/Users/yuding/.emacs.d/customizations/ui.el" . 4058)
+ ("c:/Users/yuding/.emacs.d/init.el" . 3796)
+ ("c:/Users/yuding/.emacs.d/customizations/editing.el" . 3861)
+ ("c:/Users/yuding/.emacs.d/customizations/navigation.el" . 4323))
